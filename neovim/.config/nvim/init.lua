afc19fab73101efc5fc5dbcc54094232e69775ce
Pandelis Zembashis <info@pandelis.me>
2022-11-07 21:06:01 +0000

    feat: spicy theme

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,14 +1,39 @@
 require('plugins')
+local lsp = require('lspconfig')
+
+lsp.tsserver.setup {}
 
 local neogit = require('neogit')
 
 neogit.setup {}
 
+require("catppuccin").setup {
+  highlight_overrides = {
+    all = function (colors) 
+      return {
+        LineNr = { fg = colors.mauve },
+      }
+    end,
+    },
+}
 
 vim.o.number = true
 vim.o.relativenumber = true
 vim.g.mapleader = ' '
-vim.cmd.colorscheme = "catppuccin"
+vim.cmd("colorscheme catppuccin")
+
+vim.opt.guicursor =  ''
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
 
 
 -- Functional wrapper for mapping custom keybindings
@@ -20,7 +45,17 @@ function map(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+
+
+
+
+map("n", "<Leader>ff", ":Telescope find_files<CR>")
+map("n", "<Leader>fb", ":Telescope buffers<CR>")
+
+-- quicksave
+map("n", "<Leader>s", ":update<CR>")
+
+
+-- git
 map("n", "<Leader>gs", ":Neogit<CR>", { silent = true }) 
-map("n", "<Leader>gc", ":Neogit commit<CR>", { silent = true }) 
-
-
+map("n", "<Leader>gc", ":Neogit commit<CR>", { silent =  true })
--- neovim/.config/nvim/plugin/packer_compiled.lua
+++ neovim/.config/nvim/plugin/packer_compiled.lua
@@ -84,14 +84,44 @@ _G.packer_plugins = {
     path = "/Users/pz/.local/share/nvim/site/pack/packer/start/neogit",
     url = "https://github.com/TimUntersberger/neogit"
   },
+  ["nvim-lspconfig"] = {
+    loaded = true,
+    path = "/Users/pz/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    url = "https://github.com/neovim/nvim-lspconfig"
+  },
+  ["nvim-surround"] = {
+    config = { "\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18nvim-surround\frequire\0" },
+    loaded = true,
+    path = "/Users/pz/.local/share/nvim/site/pack/packer/start/nvim-surround",
+    url = "https://github.com/kylechui/nvim-surround"
+  },
+  ["packer.nvim"] = {
+    loaded = true,
+    path = "/Users/pz/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    url = "https://github.com/wbthomason/packer.nvim"
+  },
   ["plenary.nvim"] = {
     loaded = true,
     path = "/Users/pz/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
+  },
+  ["telescope.nvim"] = {
+    loaded = true,
+    path = "/Users/pz/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  ["tokyonight.nvim"] = {
+    loaded = true,
+    path = "/Users/pz/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
+    url = "https://github.com/folke/tokyonight.nvim"
   }
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: nvim-surround
+time([[Config for nvim-surround]], true)
+try_loadstring("\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18nvim-surround\frequire\0", "config", "nvim-surround")
+time([[Config for nvim-surround]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
